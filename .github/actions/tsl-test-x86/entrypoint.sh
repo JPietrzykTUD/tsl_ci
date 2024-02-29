23cbe4eb2e8ffd0eb545aab47fb9a6d5ddf8ce1e
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2024-02-29 15:03:41 +0100

    .

--- .github/actions/tsl-test-x86/entrypoint.sh
+++ .github/actions/tsl-test-x86/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
 COMPILER=$1
+TSL_PATH=$2
 
 REPO_ROOT=/github/workspace
+TSL_ROOT=${REPO_ROOT}/${TSL_PATH}
 
 echo "name=${COMPILER}" >> $GITHUB_OUTPUT
 
@@ -14,5 +16,33 @@ echo "out=${LOG_BASE}" >> $GITHUB_OUTPUT
 
 ls ${REPO_ROOT}/tsl/intel/ -halt > ${LOG_PATH}/tsl.log 2>&1
 
-echo "msg=\"TSL can be generated build (with $COMPILER) and successfully executed.\"" >> $GITHUB_OUTPUT
+# iterate over all directories in tsl/intel
+for d in ${TSL_ROOT}/*; do
+  if [ -d "$d" ]; then
+    CURRENT_PATH=${TSL_ROOT}/${d}
+    echo "Building ${CURRENT_PATH}" >> ${LOG_PATH}/tsl.log 2>&1
+    CURRENT_LOG_PATH=${LOG_PATH}/${d}
+    mkdir -p ${CURRENT_LOG_PATH}
+    cmake -S ${CURRENT_PATH} -B ${CURRENT_PATH}/build -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_BUILD_TYPE=Release > ${CURRENT_LOG_PATH}/cmake.log 2>&1
+    if [ $? -ne 0 ]; then
+      echo "msg=cmake failed for $d" >> $GITHUB_OUTPUT
+      echo "success=false" >> $GITHUB_OUTPUT
+      exit
+    fi
+    cmake --build ${CURRENT_PATH}/build --config Release -j > ${CURRENT_LOG_PATH}/make.log 2>&1
+    if [ $? -ne 0 ]; then
+      echo "msg=Build failed for $d" >> $GITHUB_OUTPUT
+      echo "success=false" >> $GITHUB_OUTPUT
+      exit
+    fi
+    ${CURRENT_PATH}/build/src/test/tsl_test > ${CURRENT_LOG_PATH}/test.log 2>&1
+    if [ $? -ne 0 ]; then
+      echo "msg=Tests failed for $d" >> $GITHUB_OUTPUT
+      echo "success=false" >> $GITHUB_OUTPUT
+      exit
+    fi
+  fi
+done
+
+echo "msg=TSL can be generated build (with $COMPILER) and all tests were green." >> $GITHUB_OUTPUT
 echo "success=success" >> $GITHUB_OUTPUT
