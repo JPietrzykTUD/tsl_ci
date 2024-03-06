2fdcdbf17e7f9f814315beeba4e26ba844370080
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2024-03-06 09:07:28 +0100

    .

--- .github/actions/tsl-rpm-build/entrypoint.sh
+++ .github/actions/tsl-rpm-build/entrypoint.sh
@@ -6,7 +6,7 @@ VERSION=${VERSION_ORIG#v}
 TSL_TAR_GZ_NAME=$2 #tsl.tar.gz
 TSL_TAR_PREFIX=$3 #tsl/generate_tsl_
 
-RPM_BASE=/root/rpmbuild
+RPM_BASE=/github/home/rpmbuild
 SPEC_FILE=${RPM_BASE}/SPECS/tsl.spec
 
 # sed ${{ VERSION_TAG }} in tsl.spec with $VERSION
@@ -23,6 +23,9 @@ OUT_BASE=packages
 OUT=${REPO_ROOT}/${OUT_BASE}
 mkdir -p ${OUT}
 echo "out=${OUT_BASE}" >> $GITHUB_OUTPUT
+
+echo "#TSL_ROOT: ${TSL_ROOT}" >> ${SPEC_FILE}
+echo "#ls ${TSL_ROOT} -halt" >> ${SPEC_FILE}
 
 cp ${SPEC_FILE} ${OUT}
 
