492cec6c29b593e40c5f70155da7002d159696d8
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2024-03-07 22:55:44 +0100

    agagagag

--- .github/actions/tsl-test-x86/entrypoint.sh
+++ .github/actions/tsl-test-x86/entrypoint.sh
@@ -6,6 +6,11 @@ TSL_PATH=$2
 REPO_ROOT=/github/workspace
 TSL_ROOT=${REPO_ROOT}/${TSL_PATH}
 
+TSL_STRIPPED=${TSL_PATH#generate/}
+#transform TSL_STRIPPED to array by strings by splitting it with '_'
+AVAIL_FLAGS=(${LSCPU_FLAGS_STRING//_/ })
+LSCPU_FLAGS_STRING=$(LANG=en;lscpu | grep 'Flags:' | sed -E 's/Flags:\s*//g' | sed -E 's/\s/:/g')
+
 BUILD_AND_TEST_BASE=${TSL_PATH}/build_and_test/${COMPILER}
 BUILD_AND_TEST_PATH=${TSL_ROOT}/${BUILD_AND_TEST_BASE}
 
@@ -48,11 +53,22 @@ if [ $? -ne 0 ]; then
 fi
 echo "Done"
 
+# strip generate/ from TSL_PATH
+
 EXECUTABLE=${BUILD_PATH}/src/test/tsl_test
 echo "Testing (${EXECUTABLE})"
 echo "Executing ${EXECUTABLE}" >> ${LOG_FILE} 2>&1
 file ${EXECUTABLE}
 echo "file $(file ${EXECUTABLE})" >> ${LOG_FILE} 2>&1
+
+for i in "${!AVAIL_FLAGS[@]}"
+do
+  if [[ ! $LSCPU_FLAGS_STRING == *"${AVAIL_FLAGS[i]}"* ]]; then
+    echo "msg=CPU does not support all required flags for TSL_STRIPPED=${TSL_STRIPPED}" >> $GITHUB_OUTPUT
+    echo "success=skipped" >> $GITHUB_OUTPUT
+    exit
+  fi
+done
 ${EXECUTABLE} >> ${LOG_FILE} 2>&1
 if [ $? -ne 0 ]; then
   echo "msg=Tests failed for $TSL_ROOT" >> $GITHUB_OUTPUT
@@ -61,45 +77,6 @@ if [ $? -ne 0 ]; then
 fi
 echo "Done"
 
-
-
-# # iterate over all directories in tsl/intel
-# for d in ${TSL_ROOT}/*; do
-#   if [ -d "$d" ]; then    
-#     CURRENT_PATH=${d}
-#     STRIPPED_PATH=${CURRENT_PATH#${TSL_ROOT}/}
-#     echo "Building ${STRIPPED_PATH} (with ${COMPILER})"
-#     echo "Building ${CURRENT_PATH}" >> ${LOG_FILE} 2>&1
-#     CURRENT_LOG_PATH=${LOG_PATH}/${STRIPPED_PATH}
-#     mkdir -p ${CURRENT_LOG_PATH}
-#     cmake -S ${CURRENT_PATH} -B ${CURRENT_PATH}/build -DCMAKE_CXX_COMPILER=${COMPILER_BIN} -DCMAKE_BUILD_TYPE=Release >> ${CURRENT_LOG_PATH}/cmake.log 2>&1
-#     if [ $? -ne 0 ]; then
-#       echo "msg=cmake failed for $d" >> $GITHUB_OUTPUT
-#       echo "success=false" >> $GITHUB_OUTPUT
-#       exit
-#     fi
-#     cmake --build ${CURRENT_PATH}/build --config Release >> ${CURRENT_LOG_PATH}/make.log 2>&1
-#     if [ $? -ne 0 ]; then
-#       echo "msg=Build failed for $d" >> $GITHUB_OUTPUT
-#       echo "success=false" >> $GITHUB_OUTPUT
-#       exit
-#     fi
-#     echo "Done"
-#     echo "Testing (${CURRENT_PATH}/build/src/test/tsl_test)"
-#     ls -halt ${CURRENT_PATH}/build/src/test/tsl_test
-#     file ${CURRENT_PATH}/build/src/test/tsl_test
-#     echo "Executing ${CURRENT_PATH}/build/src/test/tsl_test" >> ${CURRENT_LOG_PATH}/test.log 2>&1
-#     EXECUTABLE=${CURRENT_PATH}/build/src/test/tsl_test
-#     echo "file $(file ${EXECUTABLE})" >> ${CURRENT_LOG_PATH}/test.log 2>&1
-#     ${EXECUTABLE} >> ${CURRENT_LOG_PATH}/test.log 2>&1
-#     if [ $? -ne 0 ]; then
-#       echo "msg=Tests failed for $d" >> $GITHUB_OUTPUT
-#       echo "success=false" >> $GITHUB_OUTPUT
-#       exit
-#     fi
-#     echo "Done"
-#   fi
-# done
 
 echo "msg=TSL can be generated build (with $COMPILER) and all tests were green." >> $GITHUB_OUTPUT
 echo "success=true" >> $GITHUB_OUTPUT
